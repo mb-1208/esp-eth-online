30e4b8b3b270720a53fe4fe40b3184c5096f43c3
mb-1208 <mbntngfadlillahh1212@gmail.com>
2022-08-14 14:27:30 +0700

    add link

--- src/components/Buttons/PlayOffline.js
+++ src/components/Buttons/PlayOffline.js
@@ -6,23 +6,23 @@ export const PlayOfflineButton = () => {
   const toast = useToast();
   return (
     <>
-      {/* <Link href="offline" passHref> */}
-      <button
-        className="btn-menu-style"
-        onClick={() => {
-          toast({
-            title: "Maintenance!",
-            description: "this feature still on Maintenance",
-            status: "warning",
-            duration: 5000,
-            isClosable: true,
-            position: "top",
-          });
-        }}
-      >
-        Play Offline
-      </button>
-      {/* </Link> */}
+      <Link href="https://rsp-eth.vercel.app/" passHref>
+        <button
+          className="btn-menu-style"
+          onClick={() => {
+            toast({
+              title: "Maintenance!",
+              description: "this feature still on Maintenance",
+              status: "warning",
+              duration: 5000,
+              isClosable: true,
+              position: "top",
+            });
+          }}
+        >
+          Play Offline
+        </button>
+      </Link>
     </>
   );
 };
